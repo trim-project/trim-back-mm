fb7348fbe8bae77089b5105ac53f83080effce0b
hann <wjdgks1233@gmail.com>
2024-12-13 14:30:55 +0900

    feature : create application in infra module #1

---
+++ Trim-Infra/src/main/java/trim/TrimInfraServiceApplication.java
@@ -0,0 +1,12 @@
+package trim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimInfraServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrimInfraServiceApplication.class, args);
+    }
+}
