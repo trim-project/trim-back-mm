9a85811cdad799410dbe3c680327e418ca550e3c
hann <wjdgks1233@gmail.com>
2024-12-13 14:27:21 +0900

    feature : create application.class in common module #1

---
+++ Trim-Common/src/main/java/trim/TrimCommonApplication.java
@@ -0,0 +1,11 @@
+package trim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrimCommonApplication.class, args);
+    }
+}
