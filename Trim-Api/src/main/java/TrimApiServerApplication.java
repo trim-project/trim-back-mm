b79fe328d40543153cd7180e556b2cc3e7bdf8e6
hann <wjdgks1233@gmail.com>
2024-12-13 14:22:48 +0900

    feature : create application.class in api module #1

---
+++ Trim-Api/src/main/java/TrimApiServerApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimApiServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrimApiServerApplication.class, args);
+    }
+}
