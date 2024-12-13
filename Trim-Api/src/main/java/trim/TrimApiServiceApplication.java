f5d8205144c0f776d2db9bc1dd688a0a0b32396a
hann <wjdgks1233@gmail.com>
2024-12-13 14:29:33 +0900

    feature : create application.class in domain module & refactor name of application class #1

---
+++ Trim-Api/src/main/java/trim/TrimApiServiceApplication.java
@@ -0,0 +1,11 @@
+package trim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimApiServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrimApiServiceApplication.class, args);
+    }
+}
--- Trim-Domain/src/main/java/trim/TrimDomainServiceApplication.java
+++ Trim-Domain/src/main/java/trim/TrimDomainServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrimCommonApplication {
+public class TrimDomainServiceApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(TrimCommonApplication.class, args);
+        SpringApplication.run(TrimDomainServiceApplication.class, args);
     }
 }
