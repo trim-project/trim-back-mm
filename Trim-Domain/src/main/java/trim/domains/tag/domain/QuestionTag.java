16f72b8a7ec9f8df33b0ada9a67335fa370da50b
hann <wjdgks1233@gmail.com>
2024-12-15 21:30:35 +0900

    feature : create Question entity and relations #3

---
+++ Trim-Domain/src/main/java/trim/domains/tag/domain/QuestionTag.java
@@ -0,0 +1,29 @@
+package trim.domains.tag.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import trim.common.model.BaseTimeEntity;
+import trim.domains.question.domain.Question;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class QuestionTag extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+}
