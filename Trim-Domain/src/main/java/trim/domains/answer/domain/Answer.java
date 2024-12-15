73b7c84673a8dbfa565ec8a51ce8470f7120f8df
hann <wjdgks1233@gmail.com>
2024-12-15 21:50:43 +0900

    feature : create answer entity & repo #3

---
+++ Trim-Domain/src/main/java/trim/domains/answer/domain/Answer.java
@@ -0,0 +1,29 @@
+package trim.domains.answer.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import trim.common.model.BaseTimeEntity;
+import trim.domains.member.domain.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Answer extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")
+    private Member writer;
+
+}
