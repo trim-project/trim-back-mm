f71bd23b79df01b343b4f100a07ae54b25344537
hann <wjdgks1233@gmail.com>
2025-01-01 00:44:01 +0900

    feature : create exception.code package in common module #6

---
+++ Trim-Common/src/main/java/trim/common/exception/code/ErrorStatus.java
@@ -0,0 +1,46 @@
+package trim.common.exception.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorStatus implements BaseCode{
+
+    // 서버 오류
+    _INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, 5000, "서버 에러, 관리자에게 문의 바랍니다."),
+    _UNAUTHORIZED_LOGIN_DATA_RETRIEVAL_ERROR(INTERNAL_SERVER_ERROR, 5001, "서버 에러, 로그인이 필요없는 요청입니다."),
+    _ASSIGNABLE_PARAMETER(BAD_REQUEST, 5002, "인증타입이 잘못되어 할당이 불가능합니다."),
+
+    // 일반적인 요청 오류
+    _BAD_REQUEST(BAD_REQUEST, 4000, "잘못된 요청입니다."),
+    _UNAUTHORIZED(UNAUTHORIZED, 4001, "로그인이 필요합니다."),
+    _FORBIDDEN(FORBIDDEN, 4002, "금지된 요청입니다.");
+
+    private final HttpStatus httpStatus;
+    private final Integer code;
+    private final String message;
+
+
+    @Override
+    public Reason getReason() {
+        return Reason.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .build();
+    }
+
+    @Override
+    public Reason getReasonHttpStatus() {
+        return Reason.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .build();
+    }
+}
