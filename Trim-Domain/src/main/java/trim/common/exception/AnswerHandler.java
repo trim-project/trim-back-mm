6d2ccfb2d816b87daebe2dad2d7c68167288f72c
hann <wjdgks1233@gmail.com>
2025-01-06 14:28:20 +0900

    feature : create handlers extends GeneralException #6

---
+++ Trim-Domain/src/main/java/trim/common/exception/AnswerHandler.java
@@ -0,0 +1,7 @@
+package trim.common.exception;
+
+public class AnswerHandler extends GeneralException{
+    public AnswerHandler(BaseCode code) {
+        super(code);
+    }
+}
---
+++ Trim-Domain/src/main/java/trim/common/exception/MemberHandler.java
@@ -0,0 +1,7 @@
+package trim.common.exception;
+
+public class MemberHandler extends GeneralException {
+    public MemberHandler(BaseCode code) {
+        super(code);
+    }
+}
