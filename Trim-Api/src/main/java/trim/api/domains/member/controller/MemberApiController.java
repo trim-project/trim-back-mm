d840700b561b67b1906f7bae4532b1e0e0805878
hann <wjdgks1233@gmail.com>
2025-01-06 14:20:23 +0900

    refactor : apply ApiResponseDto in MemberApiController.class #6

--- Trim-Api/src/main/java/trim/api/domains/member/controller/MemberApiController.java
+++ Trim-Api/src/main/java/trim/api/domains/member/controller/MemberApiController.java
@@ -2,6 +2,7 @@ package trim.api.domains.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import trim.api.common.dto.ApiResponseDto;
 import trim.api.domains.member.service.RegisterMemberUseCase;
 import trim.api.domains.member.vo.MemberRequest;
 
@@ -13,7 +14,7 @@ public class MemberApiController {
     private final RegisterMemberUseCase registerMemberUseCase;
 
     @PostMapping
-    public Long registerServerTestMember(@RequestBody MemberRequest memberRequest) {
-        return registerMemberUseCase.execute(memberRequest);
+    public ApiResponseDto<Long> registerServerTestMember(@RequestBody MemberRequest memberRequest) {
+        return ApiResponseDto.onSuccess(registerMemberUseCase.execute(memberRequest));
     }
 }
