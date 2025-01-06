12b9802d197c6c8fdb693c40e51305346fb6ae58
hann <wjdgks1233@gmail.com>
2025-01-06 14:19:27 +0900

    refactor : apply ApiResponseDto in QuestionCommentApiController.class #6

--- Trim-Api/src/main/java/trim/api/domains/comment/controller/QuestionCommentApiController.java
+++ Trim-Api/src/main/java/trim/api/domains/comment/controller/QuestionCommentApiController.java
@@ -3,6 +3,7 @@ package trim.api.domains.comment.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import trim.api.common.dto.ApiResponseDto;
 import trim.api.domains.comment.service.CreateQuestionCommentUseCase;
 import trim.api.domains.comment.service.DeleteQuestionCommentUseCase;
 import trim.api.domains.comment.vo.request.CreateQuestionCommentRequest;
@@ -18,18 +19,19 @@ public class QuestionCommentApiController {
     
     /** 댓글 등록 테스트용 **/
     @PostMapping
-    public Long createQuestionComment(Member writer,
-                                      @PathVariable("questionId") Long questionId,
-                                      @RequestBody CreateQuestionCommentRequest dto){
+    public ApiResponseDto<Long> createQuestionComment(Member writer,
+                                                @PathVariable("questionId") Long questionId,
+                                                @RequestBody CreateQuestionCommentRequest dto){
 
-        return createQuestionCommentUseCaseUseCase.execute(writer, questionId, dto);
+        return ApiResponseDto.onSuccess(createQuestionCommentUseCaseUseCase.execute(writer, questionId, dto));
     }
 
     /** 댓글 삭제 테스트용 **/
     @DeleteMapping("/{commentId}")
-    public void deleteQuestionComment(Member writer,
+    public ApiResponseDto<Boolean> deleteQuestionComment(Member writer,
                                       @PathVariable("commentId") Long commentId){
         deleteQuestionCommentUseCase.execute(writer, commentId);
+        return ApiResponseDto.onSuccess(true);
     }
 
 }
