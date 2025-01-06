536bc56638a85b48bda9f3a4ae3efca01b1c766e
hann <wjdgks1233@gmail.com>
2025-01-06 14:42:27 +0900

    refactor : apply handler in QuestionComment Adaptor&Validator #6

--- Trim-Domain/src/main/java/trim/domains/comment/validate/QuestionCommentValidatorImpl.java
+++ Trim-Domain/src/main/java/trim/domains/comment/validate/QuestionCommentValidatorImpl.java
@@ -2,6 +2,8 @@ package trim.domains.comment.validate;
 
 import lombok.RequiredArgsConstructor;
 import trim.common.annotation.DomainValidator;
+import trim.common.exception.ErrorStatus;
+import trim.common.exception.QuestionCommentHandler;
 import trim.domains.comment.domain.QuestionComment;
 import trim.domains.comment.repository.QuestionCommentRepository;
 import trim.domains.member.domain.Member;
@@ -15,7 +17,7 @@ public class QuestionCommentValidatorImpl implements QuestionCommentValidator{
     @Override
     public void checkCanDelete(Member writer, QuestionComment questionComment) {
         if(isNotWriter(writer, questionComment)){
-            throw new IllegalArgumentException("Is Not Writer.");
+            throw new QuestionCommentHandler(ErrorStatus.QUESTION_COMMENT_COULD_BE_TOUCHED_BY_ONLY_WRITER);
         }
     }
 
