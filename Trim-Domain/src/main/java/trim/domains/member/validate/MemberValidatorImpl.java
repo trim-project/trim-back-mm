185095b0e13ba7efa4deeab46e55e32fe8d686c6
hann <wjdgks1233@gmail.com>
2025-01-07 11:40:03 +0900

    refactor : unify member exceptions #11

--- Trim-Domain/src/main/java/trim/domains/member/validate/MemberValidatorImpl.java
+++ Trim-Domain/src/main/java/trim/domains/member/validate/MemberValidatorImpl.java
@@ -3,8 +3,7 @@ package trim.domains.member.validate;
 import lombok.RequiredArgsConstructor;
 import trim.common.annotation.DomainValidator;
 import trim.domains.member.domain.Member;
-import trim.domains.member.exception.handler.HaveAnyChanceOfConvertNicknameException;
-import trim.domains.member.exception.handler.MemberNotFoundException;
+import trim.domains.member.exception.MemberHandler;
 import trim.domains.member.repository.MemberRepository;
 
 @DomainValidator
@@ -15,14 +14,14 @@ public class MemberValidatorImpl implements MemberValidator{
     @Override
     public void checkCanRegister(String username) {
         if (!memberRepository.existsByNickname(username)) {
-            throw MemberNotFoundException.EXCEPTION;
+            throw MemberHandler.NOT_FOUND;
         }
     }
 
     @Override
     public void checkCanConvertNickname(Member member) {
         if (member.getNicknameChangeChance() == 0) {
-            throw HaveAnyChanceOfConvertNicknameException.EXCEPTION;
+            throw MemberHandler.HAVE_ANY_CHANCE_OF_CONVERT_NICKNAME;
         }
     }
 }
