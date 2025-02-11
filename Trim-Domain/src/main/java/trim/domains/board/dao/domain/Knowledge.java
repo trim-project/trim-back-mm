18484fea3cba034ebddd6d3955496015e3ed1462
hann <wjdgks1233@gmail.com>
2025-02-11 15:41:49 +0900

    feature : create knowledge dao #18

---
+++ Trim-Domain/src/main/java/trim/domains/board/dao/domain/Knowledge.java
@@ -0,0 +1,23 @@
+package trim.domains.board.dao.domain;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@DiscriminatorValue("type_knowledge")
+public class Knowledge {
+
+    @Enumerated(EnumType.STRING)
+    private MajorType majorType;
+}
