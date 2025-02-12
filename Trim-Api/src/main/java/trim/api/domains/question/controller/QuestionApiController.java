652ed825a8f5e20ab53752c6d6e7686914a6af04
hann <wjdgks1233@gmail.com>
2025-02-12 16:39:49 +0900

    feature : add get boards by pagination #18

--- Trim-Api/src/main/java/trim/api/domains/question/controller/QuestionApiController.java
+++ Trim-Api/src/main/java/trim/api/domains/question/controller/QuestionApiController.java
@@ -3,6 +3,8 @@ package trim.api.domains.question.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import trim.api.common.dto.ApiResponseDto;
 import trim.api.domains.question.vo.request.QuestionRequest;
@@ -23,23 +25,24 @@ public class QuestionApiController {
     private final EditQuestionUseCase editQuestionUseCase;
     private final GetSpecificQuestionUseCase getSpecificQuestionUseCase;
     private final GetAllQuestionUseCase getAllQuestionUseCase;
+    private final GetAllQuestionByPaginationUseCase getAllQuestionByPaginationUseCase;
 
     @Operation(summary = "질문 게시판 작성 메서드입니다.")
     @PostMapping("/members/{memberId}")
-    public ApiResponseDto<Long> createQuestion(@PathVariable Long memberId, @RequestBody QuestionRequest request){
+    public ApiResponseDto<Long> createQuestion(@PathVariable Long memberId, @RequestBody QuestionRequest request) {
         return ApiResponseDto.onSuccess(createQuestionUseCase.execute(memberId, request));
 
     }
 
     @Operation(summary = "질문 게시판 조회 메서드입니다. 질문 게시판의 pk를 통해 조회합니다.")
     @GetMapping("/{questionId}")
-    public ApiResponseDto<QuestionDetailResponse> getSpecificQuestion(@PathVariable Long questionId){
+    public ApiResponseDto<QuestionDetailResponse> getSpecificQuestion(@PathVariable Long questionId) {
         return ApiResponseDto.onSuccess(getSpecificQuestionUseCase.execute(questionId));
     }
 
     @Operation(summary = "질문 게시판 리스트 조회 메서드입니다.")
     @GetMapping
-    public ApiResponseDto<List<QuestionSummaryResponse>> getAllQuestions(){
+    public ApiResponseDto<List<QuestionSummaryResponse>> getAllQuestions() {
         return ApiResponseDto.onSuccess(getAllQuestionUseCase.execute());
     }
 
@@ -52,4 +55,13 @@ public class QuestionApiController {
         return ApiResponseDto.onSuccess(true);
     }
 
+    @Operation(summary = "질문 게시판을 모두 조회합니다. 이때 페이지네이션을 통해 n만큼의 개수만을 불러올 수 있습니다.")
+    @GetMapping("/page")
+    public ApiResponseDto<List<QuestionSummaryResponse>> getAllQuestionByPagination(
+            @RequestParam(defaultValue = "0") int currentPage,
+            @RequestParam int pageSize
+    ) {
+        Pageable pageable = PageRequest.of(currentPage, pageSize);
+        return ApiResponseDto.onSuccess(getAllQuestionByPaginationUseCase.execute(pageable));
+    }
 }
