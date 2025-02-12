a8047448619f028fe0dd74f0c499fdcc36b0ce3f
hann <wjdgks1233@gmail.com>
2025-02-12 10:00:56 +0900

    feature : create GetAllKnowledgeUseCase #18

---
+++ Trim-Api/src/main/java/trim/api/domains/knowledge/service/GetAllKnowledgeUseCase.java
@@ -0,0 +1,37 @@
+package trim.api.domains.knowledge.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import trim.api.domains.knowledge.vo.response.KnowledgeSummaryResponse;
+import trim.common.annotation.UseCase;
+import trim.domains.board.business.adaptor.KnowledgeAdaptor;
+import trim.domains.board.dao.domain.Knowledge;
+import trim.domains.comment.business.adaptor.CommentAdaptor;
+import trim.domains.like.business.adaptor.LikeAdaptor;
+import trim.domains.tag.business.adaptor.TagAdaptor;
+
+import java.util.List;
+
+@UseCase
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GetAllKnowledgeUseCase {
+
+    private final KnowledgeAdaptor knowledgeAdaptor;
+    private final TagAdaptor tagAdaptor;
+    private final LikeAdaptor likeAdaptor;
+    private final CommentAdaptor commentAdaptor;
+
+    public List<KnowledgeSummaryResponse> execute() {
+        List<Knowledge> knowledgeList = knowledgeAdaptor.queryAllKnowledge();
+        return knowledgeList.stream()
+                .map(knowledge ->
+                        KnowledgeSummaryResponse.of(
+                                knowledge,
+                                knowledge.getWriter(),
+                                likeAdaptor.queryCountByBoard(knowledge.getId()),
+                                commentAdaptor.queryCountByBoardId(knowledge.getId()),
+                                tagAdaptor.queryNamesByBoardId(knowledge.getId())))
+                .toList();
+    }
+}
