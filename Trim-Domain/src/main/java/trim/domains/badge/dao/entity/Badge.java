b5f8ba48e7a7c1f4cfff0ad82c73f17e36f9d266
hann <wjdgks1233@gmail.com>
2025-02-20 15:20:15 +0900

    feature : create badge entity #14

---
+++ Trim-Domain/src/main/java/trim/domains/badge/dao/entity/Badge.java
@@ -0,0 +1,31 @@
+package trim.domains.badge.dao.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Table(name = "badge")
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "badge_id")
+    private Long id;
+
+    @Column(name = "next_badge_id")
+    private Long nextBadgeId;
+
+    private String badgeTitle;
+
+    private int goalCount;
+
+
+}
