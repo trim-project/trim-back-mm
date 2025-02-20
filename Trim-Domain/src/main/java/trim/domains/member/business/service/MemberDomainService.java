6af4d9666bdb7e01beecf89e216d13a1a348ed33
hann <wjdgks1233@gmail.com>
2025-02-20 21:22:24 +0900

    refactor : edit registerMember in MemberDomainService #23

--- Trim-Domain/src/main/java/trim/domains/member/business/service/MemberDomainService.java
+++ Trim-Domain/src/main/java/trim/domains/member/business/service/MemberDomainService.java
@@ -1,10 +1,11 @@
 package trim.domains.member.business.service;
 
+import trim.domains.member.dao.domain.Member;
 import trim.domains.member.dao.domain.Profile;
 import trim.domains.member.dao.domain.Role;
 
 public interface MemberDomainService {
-    Long registerMember(Profile profile, Role role, String nickname);
+    Member registerMember(Profile profile, Role role, String nickname);
 
     Long convertNickname(String username, String nickname);
 
