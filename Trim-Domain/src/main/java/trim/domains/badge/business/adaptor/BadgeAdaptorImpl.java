509164eb59eb3da775343e5803d5d9c9a80112b9
hann <wjdgks1233@gmail.com>
2025-02-20 20:36:55 +0900

    feature : add methods in BadgeAdaptor #21

--- Trim-Domain/src/main/java/trim/domains/badge/business/adaptor/BadgeAdaptorImpl.java
+++ Trim-Domain/src/main/java/trim/domains/badge/business/adaptor/BadgeAdaptorImpl.java
@@ -2,11 +2,32 @@ package trim.domains.badge.business.adaptor;
 
 import lombok.RequiredArgsConstructor;
 import trim.common.annotation.Adaptor;
+import trim.domains.badge.dao.entity.Badge;
 import trim.domains.badge.dao.repository.BadgeRepository;
+import trim.domains.badge.exception.BadgeHandler;
+import trim.domains.member.dao.domain.Member;
+
+import java.util.List;
 
 @Adaptor
 @RequiredArgsConstructor
 public class BadgeAdaptorImpl implements BadgeAdaptor{
 
     private final BadgeRepository badgeRepository;
+
+    @Override
+    public List<Badge> queryAllBadge() {
+        return badgeRepository.findAll();
+    }
+
+    @Override
+    public Badge queryById(Long badgeId) {
+        return badgeRepository.findById(badgeId)
+                .orElseThrow(() -> BadgeHandler.NOT_FOUND);
+    }
+
+    @Override
+    public List<Badge> queryCompletedBadgesByMember(Member member) {
+        return badgeRepository.findBadgesByMember(member);
+    }
 }
--- Trim-Domain/src/main/java/trim/domains/badge/dao/repository/BadgeRepository.java
+++ Trim-Domain/src/main/java/trim/domains/badge/dao/repository/BadgeRepository.java
@@ -1,7 +1,15 @@
 package trim.domains.badge.dao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import trim.domains.badge.dao.entity.Badge;
+import trim.domains.member.dao.domain.Member;
+
+import java.util.List;
 
 public interface BadgeRepository extends JpaRepository<Badge, Long> {
+    @Query("SELECT DISTINCT m.badge FROM Mission m " +
+            "WHERE m.member = :member")
+    List<Badge> findBadgesByMember(@Param("member") Member member);
 }
