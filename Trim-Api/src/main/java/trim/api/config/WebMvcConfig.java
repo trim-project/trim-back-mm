c3d24097607b1d2a881dc5a3832c867678d34a4e
hann <wjdgks1233@gmail.com>
2025-02-20 18:41:08 +0900

    fix : set cors setting for network with front-end

---
+++ Trim-Api/src/main/java/trim/api/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package trim.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import trim.common.util.StaticValues;
+
+import static trim.common.util.StaticValues.CORS_FRONT_LOCAL_PATH;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    //CORS setting
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**") //CORS 적용할 URL 패턴
+                .allowedOriginPatterns(CORS_FRONT_LOCAL_PATH) //자원 공유 오리진 지정
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") //요청 허용 메서드
+                .allowedHeaders("*") //요청 허용 헤더
+                .allowCredentials(true); //요청 허용 쿠키
+
+    }
+}
--- Trim-Common/src/main/java/trim/common/util/StaticValues.java
+++ Trim-Common/src/main/java/trim/common/util/StaticValues.java
@@ -3,4 +3,5 @@ package trim.common.util;
 public class StaticValues {
     public final static String LIKE_CREATE_STATUS = "create";
     public final static String LIKE_REMOVE_STATUS = "remove";
+    public final static String CORS_FRONT_LOCAL_PATH = "http://localhost:3000";
 }
