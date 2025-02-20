ad37a523d284231a5fb1291705a960b3670f2d42
hann <wjdgks1233@gmail.com>
2025-02-20 17:18:42 +0900

    feature : setup badge init (1) #21

--- Trim-Domain/src/main/java/trim/common/init/DataInitialize.java
+++ Trim-Domain/src/main/java/trim/common/init/DataInitialize.java
@@ -4,14 +4,65 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import trim.domains.badge.dao.entity.Badge;
+import trim.domains.badge.dao.entity.BadgeContent;
+import trim.domains.badge.dao.repository.BadgeRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Transactional
 @RequiredArgsConstructor
 public class DataInitialize {
 
+    private final BadgeRepository badgeRepository;
+
     @PostConstruct
     public void setUpBadge() {
+        List<Badge> saveBadgeList = new ArrayList<>();
+        //WRITE_QUESTION
+        for (int i = 1; i <= 10; i++) {
+            saveBadgeList.add(
+                    Badge.builder()
+                            .badgeTitle("write question" + i)
+                            .badgeContent(BadgeContent.WRITE_QUESTION)
+                            .goal(i * 4)
+                            .build()
+            );
+        }
+        //WRITE_ANSWER
+        for (int i = 1; i <= 10; i++) {
+            saveBadgeList.add(
+                    Badge.builder()
+                            .badgeTitle("write answer" + i)
+                            .badgeContent(BadgeContent.WRITE_ANSWER)
+                            .goal(i * 5)
+                            .build()
+            );
+        }
+        //WRITE_KNOWLEDGE
+        for (int i = 1; i <= 10; i++) {
+            saveBadgeList.add(
+                    Badge.builder()
+                            .badgeTitle("write knowledge" + i)
+                            .badgeContent(BadgeContent.WRITE_KNOWLEDGE)
+                            .goal(i * 5)
+                            .build()
+            );
+        }
+
+        //WRITE_FREE_TALK
+        for (int i = 1; i <= 10; i++) {
+            saveBadgeList.add(
+                    Badge.builder()
+                            .badgeTitle("write free_talk" + i)
+                            .badgeContent(BadgeContent.WRITE_FREE_TALK)
+                            .goal(i * 5)
+                            .build()
+            );
+        }
+
 
     }
 }
