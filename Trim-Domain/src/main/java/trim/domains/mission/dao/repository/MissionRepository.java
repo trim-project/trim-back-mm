784daf5ed4aac0c09dd2220f1935114e700e5f8d
hann <wjdgks1233@gmail.com>
2025-02-20 21:51:48 +0900

    feature : create MissionAdaptor for add queryMissionByBadgeAndMember method #23

--- Trim-Domain/src/main/java/trim/domains/mission/dao/repository/MissionRepository.java
+++ Trim-Domain/src/main/java/trim/domains/mission/dao/repository/MissionRepository.java
@@ -1,7 +1,12 @@
 package trim.domains.mission.dao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import trim.domains.badge.dao.entity.Badge;
+import trim.domains.member.dao.domain.Member;
 import trim.domains.mission.dao.entity.Mission;
 
+import java.util.Optional;
+
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+    Optional<Mission> findByBadgeAndMember(Badge badge, Member member);
 }
