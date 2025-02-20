cf5b961291f3193631df52657d2a76e70e71143a
hann <wjdgks1233@gmail.com>
2025-02-20 21:43:23 +0900

    feature : create UpgradeBadgeLevelUseCase #23

--- Trim-Api/src/main/java/trim/api/domains/badge/controller/BadgeApiController.java
+++ Trim-Api/src/main/java/trim/api/domains/badge/controller/BadgeApiController.java
@@ -3,11 +3,10 @@ package trim.api.domains.badge.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import trim.api.common.dto.ApiResponseDto;
 import trim.api.domains.badge.service.GetAllBadgesUseCase;
+import trim.api.domains.badge.service.UpgradeBadgeLevelUseCase;
 import trim.api.domains.badge.vo.response.BadgeResponse;
 
 import java.util.List;
@@ -19,10 +18,18 @@ import java.util.List;
 public class BadgeApiController {
 
     private final GetAllBadgesUseCase getAllBadgesUseCase;
+    private final UpgradeBadgeLevelUseCase upgradeBadgeLevelUseCase;
 
     @Operation(summary = "모든 뱃지를 조회합니다.")
     @GetMapping
     public ApiResponseDto<List<BadgeResponse>> getAllBadges() {
         return ApiResponseDto.onSuccess(getAllBadgesUseCase.execute());
+    }
+
+    @Operation(summary = "미션의 단계를 업그레이드 합니다. 현재 완성한 배지를 획득합니다.")
+    @PutMapping("/{badgeId}/members/{memberId}")
+    public ApiResponseDto<Integer> upgradeBadgeLevel(@PathVariable Long badgeId,
+                                                     @PathVariable Long memberId) {
+        return ApiResponseDto.onSuccess(upgradeBadgeLevelUseCase.execute(badgeId, memberId));
     }
 }
--- Trim-Domain/src/main/java/trim/domains/badge/dao/repository/BadgeRepository.java
+++ Trim-Domain/src/main/java/trim/domains/badge/dao/repository/BadgeRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import trim.domains.badge.dao.entity.Badge;
+import trim.domains.badge.dao.entity.BadgeContent;
 import trim.domains.member.dao.domain.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BadgeRepository extends JpaRepository<Badge, Long> {
     @Query("SELECT DISTINCT m.badge FROM Mission m " +
@@ -19,4 +21,6 @@ public interface BadgeRepository extends JpaRepository<Badge, Long> {
     List<String> findAllBadgeTitles();
 
     List<Badge> findByLevel(int level);
+
+    Optional<Badge> findByBadgeContentAndLevel(BadgeContent badgeContent, int level);
 }
