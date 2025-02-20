a284834285995adfa03d01f934afc94882981398
hann <wjdgks1233@gmail.com>
2025-02-20 20:36:55 +0900

    feature : add methods in BadgeAdaptor #21

--- Trim-Domain/src/main/java/trim/domains/badge/business/adaptor/BadgeAdaptor.java
+++ Trim-Domain/src/main/java/trim/domains/badge/business/adaptor/BadgeAdaptor.java
@@ -1,4 +1,15 @@
 package trim.domains.badge.business.adaptor;
 
+import trim.domains.badge.dao.entity.Badge;
+import trim.domains.member.dao.domain.Member;
+
+import java.util.List;
+
 public interface BadgeAdaptor {
+
+    List<Badge> queryAllBadge();
+
+    Badge queryById(Long badgeId);
+
+    List<Badge> queryCompletedBadgesByMember(Member member);
 }
