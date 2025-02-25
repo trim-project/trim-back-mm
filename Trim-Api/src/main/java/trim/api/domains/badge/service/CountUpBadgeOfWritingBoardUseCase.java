4659b5063d9e5fd38a8017164fff292e917010c1
hann <wjdgks1233@gmail.com>
2025-02-25 13:41:45 +0900

    refactor : unify count up mission of writing boards #23

--- Trim-Api/src/main/java/trim/api/domains/badge/service/CountUpBadgeOfWritingBoardUseCase.java
+++ Trim-Api/src/main/java/trim/api/domains/badge/service/CountUpBadgeOfWritingBoardUseCase.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import trim.common.annotation.UseCase;
 import trim.domains.badge.dao.entity.BadgeContent;
+import trim.domains.board.dao.domain.BoardType;
 import trim.domains.member.business.adaptor.MemberAdaptor;
 import trim.domains.member.dao.domain.Member;
 import trim.domains.mission.business.adaptor.MissionAdaptor;
@@ -13,16 +14,16 @@ import trim.domains.mission.dao.entity.Mission;
 @UseCase
 @Transactional
 @RequiredArgsConstructor
-public class CountUpBadgeOfWritingQuestionUseCase {
+public class CountUpBadgeOfWritingBoardUseCase {
 
     private final MissionAdaptor missionAdaptor;
     private final MissionDomainService missionDomainService;
     private final MemberAdaptor memberAdaptor;
 
 
-    public Long execute(Long memberId) {
+    public Long execute(BadgeContent badgeContent, Long memberId) {
         Member member = memberAdaptor.queryMember(memberId);
-        Mission mission = missionAdaptor.queryMissionByBadgeContentAndMember(BadgeContent.WRITE_QUESTION, member);
+        Mission mission = missionAdaptor.queryMissionByBadgeContentAndMember(badgeContent, member);
         missionDomainService.countUp(mission);
         return mission.getId();
     }
