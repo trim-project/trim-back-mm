6b806729b02b0da7aba647e36a361b6b60b40d10
hann <wjdgks1233@gmail.com>
2025-02-25 18:04:38 +0900

    feature : create findHotQuestions method in QuestionRepository #28

--- Trim-Domain/src/main/java/trim/domains/board/dao/repository/QuestionRepository.java
+++ Trim-Domain/src/main/java/trim/domains/board/dao/repository/QuestionRepository.java
@@ -3,6 +3,7 @@ package trim.domains.board.dao.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import trim.domains.board.dao.domain.Question;
 
 import java.util.List;
@@ -11,4 +12,12 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     List<Question> findByWriterProfileUsername(String username);
 
     Page<Question> findAll(Pageable pageable);
+
+    @Query("""
+    SELECT q FROM Question q 
+    LEFT JOIN (SELECT l.boardId, COUNT(l) AS likeCount FROM Like l GROUP BY l.boardId) likes 
+    ON q.id = likes.boardId
+    ORDER BY COALESCE(likes.likeCount, 0) DESC, q.createdAt DESC
+    """)
+    Page<Question> findHotQuestions(Pageable pageable);
 }
