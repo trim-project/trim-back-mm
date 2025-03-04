c5cc6786f68de0610689fa0c0e23a986a1fdffad
hann <wjdgks1233@gmail.com>
2025-03-04 18:13:00 +0900

    feature : add search method in KnowledgeAdaptor #41

--- Trim-Domain/src/main/java/trim/domains/board/dao/repository/jpa/KnowledgeRepository.java
+++ Trim-Domain/src/main/java/trim/domains/board/dao/repository/jpa/KnowledgeRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import trim.domains.board.dao.domain.Knowledge;
+import trim.domains.board.dao.repository.querydsl.KnowledgeQueryRepository;
 import trim.domains.member.dao.domain.Member;
 
 import java.util.List;
 
-public interface KnowledgeRepository extends JpaRepository<Knowledge, Long> {
+public interface KnowledgeRepository extends JpaRepository<Knowledge, Long>, KnowledgeQueryRepository {
     List<Knowledge> findByWriter(Member writer);
 
     Page<Knowledge> findAll(Pageable pageable);
