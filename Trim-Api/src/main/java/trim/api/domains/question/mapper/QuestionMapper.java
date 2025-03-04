db385730ce549e981b783337a24e82ab0162451b
hann <wjdgks1233@gmail.com>
2025-03-04 15:03:39 +0900

    refactor : apply LocalDateTime to Long in questionResponse #42

--- Trim-Api/src/main/java/trim/api/domains/question/mapper/QuestionMapper.java
+++ Trim-Api/src/main/java/trim/api/domains/question/mapper/QuestionMapper.java
@@ -3,16 +3,12 @@ package trim.api.domains.question.mapper;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import trim.api.common.util.EnumMappingUtil;
 import trim.api.domains.question.vo.request.QuestionRequest;
 import trim.api.domains.question.vo.response.QuestionResponse;
-import trim.common.util.EnumConvertUtil;
-import trim.domains.board.dao.domain.MajorType;
 import trim.domains.board.dao.domain.Question;
 import trim.domains.board.dto.QuestionDto;
-import trim.domains.member.dao.domain.SocialType;
 
 @Mapper(componentModel = "spring", uses = EnumMappingUtil.class, builder = @Builder(disableBuilder = false))
 public interface QuestionMapper {
@@ -22,7 +18,7 @@ public interface QuestionMapper {
     @Mapping(target = "questionId", source = "question.id")
     @Mapping(target = "title", source = "question.title")
     @Mapping(target = "content", source = "question.content")
-    @Mapping(target = "createdAt", source = "question.createdAt")
+    @Mapping(target = "createdAt", source = "question.createdAt", qualifiedByName = "localDateTimeToLong")
     QuestionResponse toQuestionResponse(Question question);
 
     @Mapping(target = "majorType", qualifiedByName = "stringToMajorType")
--- Trim-Api/src/main/java/trim/api/domains/question/vo/response/QuestionResponse.java
+++ Trim-Api/src/main/java/trim/api/domains/question/vo/response/QuestionResponse.java
@@ -2,15 +2,8 @@ package trim.api.domains.question.vo.response;
 
 import lombok.Builder;
 import lombok.Getter;
-import trim.api.domains.comment.vo.response.CommentResponse;
 import trim.domains.board.dao.domain.MajorType;
-import trim.domains.board.dao.domain.Question;
 import trim.domains.board.dao.domain.ResolveStatus;
-import trim.domains.comment.dao.domain.Comment;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Getter
@@ -19,7 +12,7 @@ public class QuestionResponse {
     private final Long questionId;
     private final String title;
     private final String content;
-    private final LocalDateTime createdAt;
+    private final Long createdAt;
     private final ResolveStatus resolveStatus;
     private final MajorType majorType;
 }
