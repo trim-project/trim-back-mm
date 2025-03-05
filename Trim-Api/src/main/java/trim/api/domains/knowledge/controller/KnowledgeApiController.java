af078ce78d44908d2b91e917f80c49f87ae6593f
hann <wjdgks1233@gmail.com>
2025-03-05 12:57:51 +0900

    feature : add api of searchKnowledge #41

--- Trim-Api/src/main/java/trim/api/domains/knowledge/controller/KnowledgeApiController.java
+++ Trim-Api/src/main/java/trim/api/domains/knowledge/controller/KnowledgeApiController.java
@@ -14,6 +14,7 @@ import trim.api.domains.knowledge.vo.request.KnowledgeRequest;
 import trim.api.domains.knowledge.vo.response.KnowledgeDetailResponse;
 import trim.api.domains.knowledge.vo.response.KnowledgeListResponse;
 import trim.api.domains.knowledge.vo.response.KnowledgeSummaryResponse;
+import trim.api.domains.question.vo.response.QuestionListResponse;
 
 import java.util.List;
 
@@ -30,6 +31,7 @@ public class KnowledgeApiController {
     private final GetSpecificKnowledgeUseCase getSpecificKnowledgeUseCase;
     private final GetAllKnowledgeByPaginationUseCase getAllKnowledgeByPaginationUseCase;
     private final GetHotKnowledgeUseCase getHotKnowledgeUseCase;
+    private final SearchKnowledgeUseCase searchKnowledgeUseCase;
 
     @Operation(summary = "지식 공유 게시글을 작성합니다.")
     @PostMapping("/members/{memberId}")
@@ -65,5 +67,17 @@ public class KnowledgeApiController {
     public ApiResponseDto<List<KnowledgeSummaryResponse>> getHotKnowledge() {
         Pageable pageable = PageRequest.of(0, HOT_ISSUE_COUNT);
         return ApiResponseDto.onSuccess(getHotKnowledgeUseCase.execute(pageable));
+    }
+
+    @Operation(summary = "지식공유 게시글을 검색합니다. 이때 사용되는 항목은 학과 계열과 키워드 리스트입니다" +
+            "키워드 리스트는 태그, 제목, 컨텐츠의 내용을 확인합니다.")
+    @GetMapping("/search")
+    public ApiResponseDto<KnowledgeListResponse> searchKnowledge(@RequestParam String majorType,
+                                                                 @RequestParam List<String> keyword,
+                                                                 @RequestParam(defaultValue = "0") int currentPage,
+                                                                 @RequestParam int pageSize) {
+
+        Pageable pageable = PageRequest.of(currentPage, pageSize, PageUtil.LATEST_SORTING);
+        return ApiResponseDto.onSuccess(searchKnowledgeUseCase.execute(majorType, keyword, pageable));
     }
 }
---
+++ Trim-Api/src/main/java/trim/api/domains/knowledge/service/SearchKnowledgeUseCase.java
@@ -0,0 +1,50 @@
+package trim.api.domains.knowledge.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import trim.api.domains.knowledge.vo.response.KnowledgeListResponse;
+import trim.api.domains.knowledge.vo.response.KnowledgeSummaryResponse;
+import trim.common.annotation.UseCase;
+import trim.common.util.EnumConvertUtil;
+import trim.domains.board.business.adaptor.KnowledgeAdaptor;
+import trim.domains.board.dao.domain.Knowledge;
+import trim.domains.board.dao.domain.MajorType;
+import trim.domains.comment.business.adaptor.CommentAdaptor;
+import trim.domains.like.business.adaptor.LikeAdaptor;
+import trim.domains.tag.business.adaptor.TagAdaptor;
+
+import java.util.List;
+
+@UseCase
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SearchKnowledgeUseCase {
+
+    private final KnowledgeAdaptor knowledgeAdaptor;
+    private final LikeAdaptor likeAdaptor;
+    private final CommentAdaptor commentAdaptor;
+    private final TagAdaptor tagAdaptor;
+
+    public KnowledgeListResponse execute(String majorType, List<String> keyword, Pageable pageable) {
+        MajorType convertedMajorType = EnumConvertUtil.convert(MajorType.class, majorType);
+        Page<Knowledge> knowledgePage = knowledgeAdaptor.queryKnowledgeBySearch(convertedMajorType, keyword, pageable);
+
+        return KnowledgeListResponse.builder()
+                .knowledgeResponseList(
+                        knowledgePage.getContent().stream()
+                                .map(knowledge ->
+                                        KnowledgeSummaryResponse.of(
+                                                knowledge,
+                                                knowledge.getWriter(),
+                                                likeAdaptor.queryCountByBoard(knowledge.getId()),
+                                                commentAdaptor.queryCountByBoardId(knowledge.getId()),
+                                                tagAdaptor.queryNamesByBoardId(knowledge.getId())))
+                                .toList()
+                )
+                .page(knowledgePage.getNumber())
+                .totalPages(knowledgePage.getTotalPages())
+                .build();
+    }
+}
