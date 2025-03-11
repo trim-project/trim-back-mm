ea5b4a27da9a9d6102fb9bbdaaf8d939c2af66ec
hann <wjdgks1233@gmail.com>
2025-03-11 22:32:24 +0900

    refactor : use authUser in LikeApiController #49

--- Trim-Api/src/main/java/trim/api/domains/like/service/ClickLikeAtTheBoardUseCase.java
+++ Trim-Api/src/main/java/trim/api/domains/like/service/ClickLikeAtTheBoardUseCase.java
@@ -3,10 +3,9 @@ package trim.api.domains.like.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import trim.common.annotation.UseCase;
-import trim.common.util.StaticValues;
-import trim.domains.like.business.adaptor.LikeAdaptor;
 import trim.domains.like.business.service.LikeDomainService;
 import trim.domains.like.business.validator.LikeValidator;
+import trim.domains.member.dao.domain.Member;
 
 import static trim.common.util.StaticValues.*;
 
@@ -18,12 +17,12 @@ public class ClickLikeAtTheBoardUseCase {
     private final LikeDomainService likeDomainService;
     private final LikeValidator likeValidator;
 
-    public String execute(Long boardId, Long memberId) {
-        if (likeValidator.isExist(boardId, memberId)) {
-            likeDomainService.removeLikeByBoardAndMember(boardId, memberId);
+    public String execute(Long boardId, Member member) {
+        if (likeValidator.isExist(boardId, member.getId())) {
+            likeDomainService.removeLikeByBoardAndMember(boardId, member.getId());
             return LIKE_REMOVE_STATUS;
         }
-        likeDomainService.createLike(boardId, memberId);
+        likeDomainService.createLike(boardId, member.getId());
         return LIKE_CREATE_STATUS;
     }
 }
