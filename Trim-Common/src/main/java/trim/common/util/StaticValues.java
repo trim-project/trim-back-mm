a3d4b902811f26dc2ccc6e264f4aaa0a90fa2d90
hann <wjdgks1233@gmail.com>
2025-03-11 14:45:18 +0900

    refactor : setting permit url in SecurityConfig #47

--- Trim-Common/src/main/java/trim/common/util/StaticValues.java
+++ Trim-Common/src/main/java/trim/common/util/StaticValues.java
@@ -1,8 +1,21 @@
 package trim.common.util;
 
+import java.util.List;
+
 public class StaticValues {
     public final static String LIKE_CREATE_STATUS = "create";
     public final static String LIKE_REMOVE_STATUS = "remove";
     public final static String CORS_FRONT_LOCAL_PATH = "http://localhost:3000";
-    public static final int HOT_ISSUE_COUNT = 6;
+    public final static int HOT_ISSUE_COUNT = 6;
+    public final static List<String> PERMIT_URL = List.of("/api/access/**");
+    public final static List<String> SWAGGER_URL =
+            List.of(
+                    "/swagger-ui/**",
+                    "/swagger-ui",
+                    "/swagger-ui.html",
+                    "/swagger/**",
+                    "/swagger-resources/**",
+                    "/v3/api-docs/**",
+                    "/profile"
+            );
 }
