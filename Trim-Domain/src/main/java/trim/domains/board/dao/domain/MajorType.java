1ab7c9c0bca14df550088554a54fc46bf6662da0
hann <wjdgks1233@gmail.com>
2025-03-11 13:21:31 +0900

    refactor : edit MajorType Enum #45

--- Trim-Domain/src/main/java/trim/domains/board/dao/domain/MajorType.java
+++ Trim-Domain/src/main/java/trim/domains/board/dao/domain/MajorType.java
@@ -9,41 +9,15 @@ import java.util.Random;
 @Getter
 @RequiredArgsConstructor
 public enum MajorType implements KeyedEnum {
-    // Humanities & Social Sciences
-    LIBERAL_ARTS("Liberal Arts College"),
-    SOCIAL_SCIENCES("Social Sciences College"),
-    BUSINESS_ADMINISTRATION("Business Administration College"),
-    LAW("Law College"),
-    EDUCATION("Education College"),
+    ENGINEERING("engineering"),
+    EDUCATION("education"),
+    SOCIAL_SCIENCES("social-sciences"),
+    ARTS_PHYSICAL_EDUCATION("arts-physical-education"),
+    MEDICINE_PHARMACY("medicine-pharmacy"),
+    HUMANITIES("humanities"),
+    NATURAL_SCIENCES("natural-sciences"),
+    ETC("etc");
 
-    // Natural Sciences
-    NATURAL_SCIENCES("Natural Sciences College"),
-    LIFE_SCIENCES("Life Sciences College"),
-    VETERINARY_MEDICINE("Veterinary Medicine College"),
-    PHARMACY("Pharmacy College"),
-
-    // Engineering
-    ENGINEERING("Engineering College"),
-    INFORMATION_COMMUNICATIONS("Information & Communications College"),
-    INDUSTRIAL_ENGINEERING("Industrial Engineering College"),
-
-    // Arts & Sports
-    MUSIC("Music College"),
-    FINE_ARTS("Fine Arts College"),
-    PHYSICAL_EDUCATION("Physical Education College"),
-
-    // Medical
-    MEDICINE("Medical College"),
-    DENTISTRY("Dentistry College"),
-    NURSING("Nursing College"),
-
-    // Agriculture & Marine
-    AGRICULTURAL_LIFE_SCIENCES("Agricultural & Life Sciences College"),
-    MARINE_SCIENCES("Marine Sciences College"),
-
-    // Human Ecology
-    HUMAN_ECOLOGY("Human Ecology College"),
-    HOUSING_ENVIRONMENT("Housing Environment College");
 
     private final String key;
 
