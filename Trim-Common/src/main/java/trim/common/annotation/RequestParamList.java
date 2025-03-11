af7c79dd18bdc1a2b8f88521184eb2cfa89d548b
hann <wjdgks1233@gmail.com>
2025-03-11 13:29:12 +0900

    feature : create RequestParamList annotation for split requestParam #45

---
+++ Trim-Common/src/main/java/trim/common/annotation/RequestParamList.java
@@ -0,0 +1,13 @@
+package trim.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestParamList {
+    String value(); // 원래 `@RequestParam`에서 쓰이는 값
+    String delimiter() default "\\+"; // 기본 구분자 `+`
+}
