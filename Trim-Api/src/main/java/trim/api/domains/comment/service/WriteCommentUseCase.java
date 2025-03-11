b45b8f383d5a80ebf4614ed3a2aea6b9f46c6ef2
hann <wjdgks1233@gmail.com>
2025-03-11 22:35:37 +0900

    refactor : use authUser in CommentApiController #49

--- Trim-Api/src/main/java/trim/api/domains/comment/service/WriteCommentUseCase.java
+++ Trim-Api/src/main/java/trim/api/domains/comment/service/WriteCommentUseCase.java
@@ -2,9 +2,9 @@ package trim.api.domains.comment.service;
 
 import lombok.RequiredArgsConstructor;
 import trim.common.annotation.UseCase;
-import trim.domains.board.business.adaptor.QuestionAdaptor;
 import trim.domains.comment.business.service.CommentDomainService;
 import trim.domains.member.business.adaptor.MemberAdaptor;
+import trim.domains.member.dao.domain.Member;
 
 
 @UseCase
@@ -14,9 +14,9 @@ public class WriteCommentUseCase {
     private final CommentDomainService commentDomainService;
     private final MemberAdaptor memberAdaptor;
 
-    public Long execute(Long memberId, Long questionId, String content){
+    public Long execute(Member member, Long questionId, String content){
         return commentDomainService.createComment(
-                memberAdaptor.queryMember(memberId),
+                member,
                 questionId,
                 content
         ).getId();
