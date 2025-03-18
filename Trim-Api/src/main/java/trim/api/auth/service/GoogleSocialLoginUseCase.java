eb1cfe34e4d9f1a37e55dae42d38782e7e458783
hann <wjdgks1233@gmail.com>
2025-03-18 15:44:11 +0900

    feature : create GoogleSocialRegisterUserUseCase #53

--- Trim-Api/src/main/java/trim/api/auth/service/GoogleSocialLoginUseCase.java
+++ Trim-Api/src/main/java/trim/api/auth/service/GoogleSocialLoginUseCase.java
@@ -3,7 +3,7 @@ package trim.api.auth.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import trim.api.auth.dto.GoogleUserInfoDto;
-import trim.api.auth.helper.GoogleOauthHelper;
+import trim.api.auth.service.helper.GoogleOauthHelper;
 import trim.api.common.security.dto.JwtToken;
 import trim.api.common.security.service.TokenService;
 import trim.common.annotation.UseCase;
@@ -14,7 +14,7 @@ import trim.outer.oauth.google.vo.response.GoogleTokenResponse;
 @UseCase
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class SocialLoginUseCase {
+public class GoogleSocialLoginUseCase {
 
     private final GoogleOauthHelper googleOauthHelper;
     private final MemberAdaptor memberAdaptor;
