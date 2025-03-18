009f874e64e389f96ed768b1c1c2f2dfb11a2f21
hann <wjdgks1233@gmail.com>
2025-03-19 00:08:02 +0900

    feature : create KakaoOauthClient in Infra #54

---
+++ Trim-Infra/src/main/java/trim/outer/oauth/kakao/client/KakaoOauthClient.java
@@ -0,0 +1,23 @@
+package trim.outer.oauth.kakao.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import trim.outer.oauth.google.vo.response.GoogleTokenResponse;
+
+@FeignClient(
+        value = "kakaoOauthClient",
+        url = "https://kauth.kakao.com/oauth"
+)
+public interface KakaoOauthClient {
+
+    @PostMapping(value = "/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    GoogleTokenResponse googleOauth(
+            @RequestParam("grant_type") String grantType,
+            @RequestParam("client_id") String clientId,
+            @RequestParam("client_secret") String clientSecret,
+            @RequestParam("redirect_uri") String redirectUri,
+            @RequestParam("code") String code
+    );
+}
