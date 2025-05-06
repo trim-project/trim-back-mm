7cbdf68b591abb91c3f66b7c849fc368c600555c
hann <wjdgks1233@gmail.com>
2025-05-06 19:22:22 +0900

    feature : create DataBucketUtil class in API module #60

---
+++ Trim-Api/src/main/java/trim/api/common/util/DataBucketUtil.java
@@ -0,0 +1,41 @@
+package trim.api.common.util;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DataBucketUtil {
+
+    @Value("${spring.cloud.gcp.storage.bucket}")
+    private String bucketName;
+
+    private final Storage storage;
+
+    public String uploadImage(MultipartFile file) throws IOException {
+
+        // 이미지 uuid와 파일 형식
+        String uuid = UUID.randomUUID().toString();
+        String ext = file.getContentType();
+        log.info("uuid: {}, ext: {}", uuid, ext);
+
+        // Google Cloud Storage에 이미지 업로드
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, uuid)
+                .setContentType(ext)
+                .build();
+        log.info("blobInfo: {}", blobInfo);
+        Blob blob = storage.create(blobInfo, file.getBytes());
+        log.info("blob: {}", blob);
+        return bucketName + "/" + uuid;
+    }
+}
