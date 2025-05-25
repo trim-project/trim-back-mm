a9042accaa4e3aa54902e6259d1ba904fa71cc56
kimdanha <lily034686@gmail.com>
2025-05-25 21:04:53 +0900

    feature: purchase cloth usecase #72

--- Trim-Api/src/main/java/trim/api/domains/avatar/service/possessed/PurchaseClothUseCase.java
+++ Trim-Api/src/main/java/trim/api/domains/avatar/service/possessed/PurchaseClothUseCase.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import trim.common.annotation.UseCase;
 import trim.domains.avatar.business.adaptor.item.ClothAdaptor;
+import trim.domains.avatar.business.adaptor.possessed.PossessedClothAdaptor;
 import trim.domains.avatar.business.service.item.ClothDomainService;
 import trim.domains.avatar.dao.entity.item.Cloth;
 import trim.domains.avatar.dao.entity.possessed.PossessedCloth;
@@ -16,9 +17,13 @@ public class PurchaseClothUseCase {
 
     private final ClothDomainService clothDomainService;
     private final ClothAdaptor clothAdaptor;
+    private final PossessedClothAdaptor possessedClothAdaptor;
 
     public Long execute(Member member, Long clothId) {
         Cloth cloth = clothAdaptor.queryByClothId(clothId);
+        if(possessedClothAdaptor.queryByPossessedClothId(cloth, member)==null) {
+            member.usePoint(cloth.getPrice());
+        }
         PossessedCloth possessedCloth = clothDomainService.purchaseCloth(member, cloth);
         return possessedCloth.getId();
     }
