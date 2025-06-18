a3895a16cadfdcec04d18cd1c7b2c909fb86745b
hann <wjdgks1233@gmail.com>
2025-06-18 16:59:51 +0900

    feat: complete task #85

--- Trim-Api/src/main/java/trim/api/domains/member/controller/MemberApiController.java
+++ Trim-Api/src/main/java/trim/api/domains/member/controller/MemberApiController.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import trim.api.common.dto.ApiResponseDto;
+import trim.api.domains.member.service.GetMemberProfileUseCase;
+import trim.api.domains.member.service.PatchMemberProfileUseCase;
 import trim.api.domains.member.vo.request.MemberProfileRequest;
 import trim.api.domains.member.vo.response.MemberProfileResponse;
 import trim.api.domains.member.vo.response.MemberResponse;
@@ -18,16 +20,19 @@ import trim.domains.member.dao.domain.Member;
 @RequestMapping("/api/members")
 public class MemberApiController {
 
+    private final GetMemberProfileUseCase getMemberProfileUseCase;
+    private final PatchMemberProfileUseCase patchMemberProfileUseCase;
+
     @Operation(summary = "멤버 프로필 정보를 조회합니다. 이름, 닉네임, 전공, 학교, 한줄 소개가 들어갑니다..")
     @GetMapping
     public ApiResponseDto<MemberProfileResponse> getMemberProfile(@Parameter(hidden = true) @AuthUser Member member) {
-        return ApiResponseDto.onSuccess(null);
+        return ApiResponseDto.onSuccess(getMemberProfileUseCase.execute(member));
     }
 
     @Operation(summary = "멤버 프로필 정보를 수정합니다.")
     @PatchMapping
     public ApiResponseDto<Long> patchMemberProfile(@Parameter(hidden = true) @AuthUser Member member,
                                                    @RequestBody MemberProfileRequest memberProfileRequest) {
-        return ApiResponseDto.onSuccess(null);
+        return ApiResponseDto.onSuccess(patchMemberProfileUseCase.execute(member, memberProfileRequest));
     }
 }
