e94d95f61066a354a364a6fbc16ac936c0c9d866
hann <wjdgks1233@gmail.com>
2025-06-19 15:26:44 +0900

    refactor : EyesForURL & MouthForURL key convert #84

--- Trim-Domain/src/main/java/trim/domains/avatar/dao/entity/enums/EyesForURL.java
+++ Trim-Domain/src/main/java/trim/domains/avatar/dao/entity/enums/EyesForURL.java
@@ -8,18 +8,18 @@ import trim.common.interfaces.KeyedEnum;
 @RequiredArgsConstructor
 public enum EyesForURL implements KeyedEnum {
 
-    EYES_1("eyes_1_image"),
-    EYES_2("eyes_2_image"),
-    EYES_3("eyes_3_image"),
-    EYES_4("eyes_4_image"),
-    EYES_5("eyes_5_image"),
-    EYES_6("eyes_6_image"),
-    EYES_7("eyes_7_image"),
-    EYES_8("eyes_8_image"),
-    EYES_9("eyes_9_image"),
-    EYES_10("eyes_10_image"),
-    EYES_11("eyes_11_image"),
-    EYES_12("eyes_12_image");
+    EYES_1("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_1.svg"),
+    EYES_2("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_2.svg"),
+    EYES_3("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_3.svg"),
+    EYES_4("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_4.svg"),
+    EYES_5("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_5.svg"),
+    EYES_6("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_6.svg"),
+    EYES_7("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_7.svg"),
+    EYES_8("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_8.svg"),
+    EYES_9("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_9.svg"),
+    EYES_10("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_10.svg"),
+    EYES_11("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_11.svg"),
+    EYES_12("https://storage.googleapis.com/trim-cloud-bucket/Avatar_eyes_12.svg");
 
     private final String key;
 }
