2541322b3db1b1c3514f80d7a6bcd0b280d4d7f1
hann <wjdgks1233@gmail.com>
2025-06-19 15:23:03 +0900

    refactor : ClothForURL key convert #84

--- Trim-Domain/src/main/java/trim/domains/avatar/dao/entity/enums/ClothAvatarNumber.java
+++ Trim-Domain/src/main/java/trim/domains/avatar/dao/entity/enums/ClothAvatarNumber.java
@@ -10,8 +10,8 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public enum ClothAvatarNumber implements KeyedEnum {
     R1("r1"), R2("r2"), R3("r3"), R4("r4"),
-    R5("r5"), R6("r6"), R7("r7"), R8("r8"),
-    R9("r9"), R10("r10"), R11("r11"), R12("r12");
+    R5("r5"), R7("r7"), R9("r9"), R10("r10"),
+    R11("r11"), R12("r12");
 
     private final String key;
 
