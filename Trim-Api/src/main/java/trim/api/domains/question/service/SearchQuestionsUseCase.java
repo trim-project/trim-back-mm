c0a19bb31074cac474aaba82e1bdafe6683ee9a9
hann <wjdgks1233@gmail.com>
2025-06-23 16:44:26 +0900

    hotfix : edit search question(not include storedAvatarResponse)

--- Trim-Api/src/main/java/trim/api/domains/question/service/SearchQuestionsUseCase.java
+++ Trim-Api/src/main/java/trim/api/domains/question/service/SearchQuestionsUseCase.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import trim.api.domains.avatar.vo.response.stored.StoredAvatarResponse;
 import trim.api.domains.member.mapper.MemberMapper;
 import trim.api.domains.question.mapper.QuestionMapper;
 import trim.api.domains.question.vo.response.QuestionListResponse;
@@ -52,6 +53,13 @@ public class SearchQuestionsUseCase {
         return QuestionSummaryResponse.builder()
                 .questionResponse(QuestionMapper.INSTANCE.toQuestionResponse(question))
                 .memberResponse(MemberMapper.INSTANCE.toMemberResponse(question.getWriter()))
+                .storedAvatarResponse(StoredAvatarResponse.builder()
+                        .backgroundColor(question.getWriter().getAvatar().getBackgroundColor())
+                        .clothForURL(question.getWriter().getAvatar().getClothForURL())
+                        .mouthForURL(question.getWriter().getAvatar().getMouthForURL())
+                        .eyesForURL(question.getWriter().getAvatar().getEyesForURL())
+                        .hairForURL(question.getWriter().getAvatar().getHairForURL())
+                        .build())
                 .likeCount(likeAdaptor.queryCountByBoard(question.getId()))
                 .answerCount(answerAdaptor.queryCountByQuestionId(question.getId()))
                 .tagList(tags)
